fb29776aa3004c0729930cc11d874580b50cc229
mattxdev <mattxdev@posteo.de>
2020-04-24 23:07:51 +0200

    Exercise 3.1: XSJS and XSODATA

--- xsjs/server.js
+++ xsjs/server.js
@@ -7,7 +7,7 @@ var port  = process.env.PORT || 3000;
 
 var options = {
 //	anonymous : true, // remove to authenticate calls
-	auditLog : { logToConsole: true }, // change to auditlog service for productive scenarios
+//	auditLog : { logToConsole: true }, // change to auditlog service for productive scenarios
 	redirectUrl : "/index.xsjs"
 };
 
