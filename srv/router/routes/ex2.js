3d4cf7e0e34c8b28ded62ace7ae62323e4c7c1ec
mattxdev <mattxdev@posteo.de>
2020-05-18 11:23:11 +0200

    Exercise 4.3: HANA Database Access from Node.js

--- srv/router/routes/ex2.js
+++ srv/router/routes/ex2.js
@@ -105,6 +105,45 @@ module.exports = () => {
 			}
 		]);
 	});
+	
+	// Simple Database Select via Client Wrapper/Middleware - Promises
+	app.get("/promises",(req,res)=>{
+		const dbClass = require(global.__base + "utils/dbPromises");
+		let db = new dbClass(req.db);
+		db.preparePromisified(`SELECT SESSION_USER, CURRENT_SCHEMA FROM "DUMMY"`)
+			.then(statement => { 
+				db.statementExecPromisified(statement, [])
+					.then(results => {
+						let result = JSON.stringify({
+							Objects: results
+						});
+						return res.type("application/json").status(200).send(result);
+					})
+					.catch(err => {
+						return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+					});
+				})
+		
+			.catch(err => {
+				return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+			});
+	});
+		
+	// Simple Database Select via Client Wrapper/Middleware - Await
+	app.get("/await",async(req,res) => {
+		try {
+			const dbClass = require(global.__base + "utils/dbPromises");
+			let db = new dbClass(req.db);
+			const statement = await	db.preparePromisified(`SELECT SESSION_USER, CURRENT_SCHEMA FROM "DUMMY"`)
+			const results = await db.statementExecPromisified(statement, []);
+			let result = JSON.stringify({
+				Objects: results
+			});				
+			return res.type("application/json").status(200).send(result);
+		} catch (e) {
+			return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+		}
+	});
 
 	return app;
 };
