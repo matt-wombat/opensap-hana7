69653bcf1a523572864a2a033644075fd0c39263
mattxdev <mattxdev@posteo.de>
2020-06-03 20:43:24 +0200

    Exercise 4.4: Asynchronous Non-Blocking I/O

--- srv/router/index.js
+++ srv/router/index.js
@@ -5,4 +5,5 @@
 module.exports = (app, server) => {
 	app.use("/node", require("./routes/myNode")());
 	app.use("/node/ex2",require("./routes/ex2")());
+	app.use("/node/excAsync",require("./routes/exerciseAsync")(server));
 };
